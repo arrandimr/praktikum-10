d4b065028f434c4bdf283dd018870bad901d6704
arrandimr <61189026+arrandimr@users.noreply.github.com>
2020-04-17 11:30:47 +0700

    Update simpan.php

--- simpan.php
+++ simpan.php
@@ -5,7 +5,7 @@ $username="root";
 $password=""; 
 
 $conn = mysqli_connect($host,$username,$password,$database);
-$sql = "insert into daftarmhs (nisn, nama, email, tglregis, no_telp, jeniskel, nik, tempat_lahir, tgl_lahir, no_akta, kewarganegaraan, agama, anak, kebutuhan_khusus, alamat, rt, rw, dusun, desa, kecamatan, kdpos, kepemilikan, transport, no_kks, kps, no_kps) values ('".$nisn."', '".$nama."','".$email."','".$tanggal."','".$telp."','".$jeniskel."', '".$nik."','".$temlahir."','".$tgllahir."','".$akta."','".$kwn."', '".$agama."','".$anak."','".$kkhusus."','".$alamat."','".$rt."', '".$rw."','".$dusun."','".$desa."','".$kecamatan."','".$kdpos."', '".$kepemilikan."','".$transport."','".$kks."','".$kps."','".$nokps."')";
+$sql = "insert into daftarmhs (nisn, nama, email, tglregis, no_telp, jeniskel, nik, tempat_lahir, tgl_lahir, no_akta, kewarganegaraan, negara, agama, anak, kebutuhan_khusus, jeniskebutuhan, alamat, rt, rw, dusun, desa, kecamatan, kdpos, kepemilikan, transport, no_kks, kps, no_kps) values ('".$nisn."','".$nama."','".$email."','".$tanggal."','".$telp."','".$jeniskel."','".$nik."','".$temlahir."','".$tgllahir."','".$akta."','".$kwn."','".$negara."','".$agama."','".$anak."','".$kkhusus."','".$jeniskkhusus."','".$alamat."','".$rt."','".$rw."','".$dusun."','".$desa."','".$kecamatan."','".$kdpos."', '".$kepemilikan."','".$transport."','".$kks."','".$kps."','".$nokps."')";
 
 if (mysqli_query($conn, $sql)){} 
 else {
